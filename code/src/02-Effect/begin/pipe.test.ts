b75629f9fc0f0899b056593d1359c120d131e4a2
Olarn U. <olarn.u@odds.team>
2025-11-05 11:48:11 +0700

    Complete Function Composition labs.

---
+++ code/src/02-Effect/begin/pipe.test.ts
@@ -0,0 +1,33 @@
+import { flow } from "effect/Function"
+// import { 
+//   increment, 
+//   toString, 
+//   pipeIncrementThenToString,
+//   flowIncrementThenToString } from "./pipe"
+
+describe('incrementThenToString', () => {
+  it('should increment the number and then convert it to a string with pipe', () => {
+    //TODO: Implement pipeIncrementThenToString with Effect-pipe then uncomment the test.
+    
+    // expect(
+    //   pipeIncrementThenToString(1)
+    // ).toEqual('2')
+  })
+
+  it('should increment the number and then convert it to a string with flow', () => {
+    //TODO: Implement pipeIncrementThenToString with Effect-flow then uncomment the test.
+
+    // expect(
+    //   flowIncrementThenToString(1)
+    // ).toEqual('2')
+  })
+
+  it('should work when reuse increment and flow functions with my own flow', () => {
+    //TODO: Implement my own flow with Effect-flow then uncomment the test.
+
+    // const myFlow = flow(increment, toString)
+    // expect(
+    //   myFlow(1)
+    // ).toEqual('2')
+  })
+})
