cd6efe4384401765ccc1cc7f375698bf8585cd96
Olarn U. <olarn.u@odds.team>
2025-11-05 16:12:44 +0700

    Add currying functions and tests for phone number normalization and international conversion.

---
+++ code/src/03-Currying/solution/currying.ts
@@ -0,0 +1,9 @@
+export const normalizePhoneNumber = (s: string) =>
+  s.trim().replace(/^0/, "")
+
+export const convertToInternational = (countryCode: string, digits: string) =>
+  `+${countryCode}${digits}`
+
+export const toInternationalPhoneNumber = (countryCode: string) =>
+  (digits: string) =>
+    convertToInternational(countryCode, digits)
