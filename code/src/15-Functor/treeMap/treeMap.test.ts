ebfd2350be390b0f622f2472ab32b9fa0f2a15f2
Olarn U. <olarn.u@odds.team>
2026-01-14 15:10:13 +0700

    Add Tree Functor example.

--- code/src/15-Functor/treeMap/treeMap.test.ts
+++ code/src/15-Functor/treeMap/treeMap.test.ts
@@ -1,5 +1,37 @@
-describe('Functor treeMap', () => {
-  it('should map over a tree with an arbitrary function', () => {
+import { Tree } from "../../04-Recursion/tree"
+import { TreeF } from "./treeMap"
 
+describe('Functor treeMap', () => {
+  const increment = (x: number) => x + 1
+  const toString = (x: number) => `${x}`
+
+  const tree: Tree<number> = {
+    _tag: 'branch', value: 100,
+    left: { _tag: 'leaf', value: 200 },
+    right: { _tag: 'leaf', value: 300 }
+  }
+    
+  it('should map over a tree with an arbitrary function', () => {
+    expect(
+      (new TreeF(tree)).fmap(increment)
+    ).toEqual(new TreeF(
+      {
+        _tag: 'branch', value: 101,
+        left: { _tag: 'leaf', value: 201 },
+        right: { _tag: 'leaf', value: 301 }
+      }
+    ))
+  })
+
+  it('should map over a tree with a toString function', () => {
+    expect(
+      new TreeF(tree).fmap(toString)
+    ).toEqual(new TreeF(
+      {
+        _tag: 'branch', value: '100',
+        left: { _tag: 'leaf', value: '200' },
+        right: { _tag: 'leaf', value: '300' }
+      }
+    ))
   })
 })
