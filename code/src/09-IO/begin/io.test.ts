64990d44957b0eaecf3ead5a3d355bfc1b832c58
Olarn U. <olarn.u@odds.team>
2026-02-03 09:14:45 +0700

    Add IO begin labs.

---
+++ code/src/09-IO/begin/io.test.ts
@@ -0,0 +1,9 @@
+import { Effect, Console } from "effect"
+import { pipe } from "effect/Function"
+
+
+describe("io", () => {
+  it("should be able to console log", () => {
+
+  });
+});
---
+++ code/src/09-IO/begin/ioEither.test.ts
@@ -0,0 +1,13 @@
+import { Effect, Console } from "effect"
+import { pipe } from "effect/Function"
+
+
+describe("ioEither", () => {
+  it("should be able to read string from console and print it", () => {
+
+  })
+
+  it("should be able to read string from console", () => {
+
+  })
+})
