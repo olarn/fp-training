ae53b9b43c2eb996a0b8050b060f8808dffffae2
Olarn U. <olarn.u@odds.team>
2026-02-06 10:10:07 +0700

    Update ts-jest dependency to version 29.4.6 in package.json

--- code/src/app.ts
+++ code/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello, Functional Programming.")
+// import { IO } from "fp-ts/IO"
+
+// type Book = {
+//   id: number
+//   title: string
+//   author: string
+// }
+
+// function fetchBooks(): IO<Promise<Book[]>> {
+//   return () => fetch("https://jsonplaceholder.typicode.com/posts")
+//     .then(res => res.json())
+//     .then(data => data as Book[])
+// }
+
+// fetchBooks().run()
