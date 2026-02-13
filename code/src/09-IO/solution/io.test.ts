2123cb7fe5713feb6df51fa9dc07cd0f7396ebe5
Olarn U. <olarn.u@odds.team>
2026-02-13 11:23:14 +0700

    Add IO and IOEither implementations with corresponding tests.

--- code/src/09-IO/solution/io.test.ts
+++ code/src/09-IO/solution/io.test.ts
@@ -1,17 +1,5 @@
-import { Effect, Console } from "effect"
-import { pipe } from "effect/Function"
-
-const readLine: Effect.Effect<string> = Effect.succeed("OK")
-
-const printLine = (message: string): Effect.Effect<void> =>
-  Console.log(message)
-
-const program = pipe(
-  readLine,
-  Effect.map(str => `You entered: ${str}`),
-  Effect.tap(message => printLine(message))  
-  // tap() Runs a side effect with the result of an effect without changing the original value.
-)
+import { Effect } from "effect";
+import { program } from "./io";
 
 describe("io", () => {
   it("should be able to console log", () => {
--- code/src/09-IO/solution/ioEither.test.ts
+++ code/src/09-IO/solution/ioEither.test.ts
@@ -1,42 +1,17 @@
-import { Effect, Console } from "effect"
-import { pipe } from "effect/Function"
-
-type IOEither<E, A> = Effect.Effect<A, E>
-
-const rightReadLine: IOEither<string, string> = Effect.succeed('OK')
-const leftReadLine: IOEither<string, string> = Effect.fail('Error')
-
-const printLine = (message: string): IOEither<never, void> => Console.log(message)
-const printHello: IOEither<never, void> = printLine('Hello')
-
-const program = pipe(
-  printHello,
-  Effect.flatMap(() => rightReadLine),
-  Effect.map(str => `You entered: ${str}`),
-  Effect.tap(message => printLine(message)),
-  Effect.match({
-    onFailure: () => 'Error',
-    onSuccess: (message) => message
-  })
-)
-
-const program2 = pipe(
-  printHello,
-  Effect.flatMap(() => leftReadLine),
-  Effect.map(str => `You entered: ${str}`),
-  Effect.tap(message => printLine(message)),
-  Effect.match({
-    onFailure: () => 'Error',
-    onSuccess: (message) => message
-  })
-)
+import { Effect } from "effect"
+import { IOEither, program } from "./ioEither"
 
 describe("ioEither", () => {
+  const rightReadLine: IOEither<string, string> = Effect.succeed('OK')
+  const leftReadLine: IOEither<string, string> = Effect.fail('Error')
+
   it("should be able to read string from console and print it", () => {
-    expect(Effect.runSync(program)).toEqual('You entered: OK')
+    expect(
+      Effect.runSync(program(rightReadLine))
+    ).toEqual('You entered: OK')
   })
 
   it("should be able to read string from console", () => {
-    expect(Effect.runSync(program2)).toEqual('Error')
+    expect(Effect.runSync(program(leftReadLine))).toEqual('Error')
   })
 })
