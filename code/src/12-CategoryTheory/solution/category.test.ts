a0ff053e4b696f77b69a80a787ebe823436f332b
Olarn U. <olarn.u@odds.team>
2026-02-13 11:21:37 +0700

    Add code coverage.

--- code/src/12-CategoryTheory/solution/category.test.ts
+++ code/src/12-CategoryTheory/solution/category.test.ts
@@ -4,23 +4,26 @@ import { findLength, isGreaterThan5, convertToShortOrLong } from './category'
 describe('Category Theory', () => {
   it('proof composition definition law', () => {
     // h = g . f 
-    const g_after_f = isGreaterThan5(findLength('tiny'))
-    const h = flow(findLength, isGreaterThan5)('tiny')
+    const g_after_f = isGreaterThan5(findLength('longer_string'))
+    const h = flow(findLength, isGreaterThan5)('longer_string')
     expect(h).toEqual(g_after_f)
   })
 
   it('proof composition association', () => {
     // ( h . g ) . f 
-    const h_after_g_after_f = flow(findLength, flow(isGreaterThan5, convertToShortOrLong))
+    const h_after_g_after_f = flow(
+      findLength, 
+      flow(isGreaterThan5, convertToShortOrLong)
+    )
     // h . ( g . f )
-    const h_after_g_and_f = flow(flow(findLength, isGreaterThan5), convertToShortOrLong)
+    const h_after_g_and_f = flow(
+      flow(findLength, isGreaterThan5), 
+      convertToShortOrLong
+    )
 
     // ( h . g ) . f = h . ( g . f )
-    expect(
-      h_after_g_after_f('tiny')
-    ).toEqual(
-      h_after_g_and_f('tiny')
-    )
+    expect(h_after_g_after_f('tiny')).toEqual(h_after_g_and_f('tiny'))
+    expect(h_after_g_after_f('longer_string')).toEqual(h_after_g_and_f('longer_string'))
   })
 
   it('proof composition identity', () => {
