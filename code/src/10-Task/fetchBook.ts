80817ebdbffac4d70bc0de0ae0671bb3271424fe
Olarn U. <olarn.u@odds.team>
2026-02-19 17:15:06 +0700

    Refactor fetchBooks implementation and update app.ts to use ioFetchBooks for better IO handling.

--- code/src/10-Task/fetchBook.ts
+++ code/src/10-Task/fetchBook.ts
@@ -1,4 +1,4 @@
-import { IO, of } from "fp-ts/lib/IO"
+import { IO } from "fp-ts/lib/IO"
 
 type Book = {
   id: number
@@ -6,10 +6,22 @@ type Book = {
   author: string
 }
 
-const fetchBooks = (): IO<Promise<Book[]>> => {
-  return () => (fetch("https://jsonplaceholder.typicode.com/posts")
+export const fetchBooks = (): Promise<Book[]> =>
+  fetch("https://jsonplaceholder.typicode.com/posts")
     .then(res => res.json())
-    .then(data => data as Book[]))
-}
+    .then(data => data as Book[])
 
-fetchBooks()().then(books => console.log(books))
+
+// fetchBooks().then(books =>
+//   console.log(books[0])
+// )
+
+export const ioFetchBooks = (): IO<Promise<Book[]>> =>
+  () => fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => data as Book[])
+
+// const FetchBooks2 = ioFetchBooks()
+// FetchBooks2().then(books =>
+//   console.log(books.length)
+// )
--- code/src/app.ts
+++ code/src/app.ts
@@ -1,12 +1,1 @@
-type IO<A> = () => A
-
-const ioIncrement = (value: number): IO<number> => {
-  const output = value + 1
-  return () => {
-    console.log(output)
-    return output
-  }
-}
-
-const incrementAndLog = ioIncrement(5)
-incrementAndLog()
+console.log("Hello, Functional Programming")
