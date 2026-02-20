4f9471e91b27954adc5d3918777f5aeacab96402
Olarn U. <olarn.u@odds.team>
2026-02-20 09:29:02 +0700

    Add IO example into app.ts.

--- code/src/app.ts
+++ code/src/app.ts
@@ -1,1 +1,15 @@
-console.log("Hello, Functional Programming")
+import { pipe } from "effect/Function"
+
+type IO<A> = () => A
+const ioHello = (message: string): IO<string> => 
+  () => {
+    console.log(message)
+    return message
+  }
+
+const sayHello = pipe(
+  'Hello, Functional Programming',
+  ioHello
+)
+
+sayHello()
